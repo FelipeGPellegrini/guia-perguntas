5d9e0b36e3584284f3b2ade24266afed80ab7e0c
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-12-11 21:36:20 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs")
+
+
+app.get("/:nome/:lang", (req, res) => {
+    let nome = req.params.nome;
+    let lang = req.params.lang;
+    res.render("index", {
+        nome,
+        lang,
+        empresa: "Guia do programador",
+        inscritos: 8000
+    })
+})
+
+app.listen(8080, () => {
+    console.log("App rodando!!")
+})
