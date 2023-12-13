5a1d315df9627270e22291259504d52efe2e00e8
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-12-12 22:08:08 -0300

    condicional no ejs e repeticao

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ app.set("view engine", "ejs")
 app.get("/:nome/:lang", (req, res) => {
     let nome = req.params.nome;
     let lang = req.params.lang;
+    let exibirMsg = true;
+    let produtos = [
+        {nome: "Doritos",preco: 3.14},
+        {nome: "Coca-cola",preco:5},
+        {nome: "Leite",preco:1.45},
+        {nome: "Carne", preco:15},
+        {nome: "Redbull", preco: 6},
+        {nome: "Nescau", preco: 4}
+    ]
     res.render("index", {
         nome,
         lang,
         empresa: "Guia do programador",
-        inscritos: 8000
+        inscritos: 8000,
+        exibirMsg,
+        produtos
     })
 })
 
