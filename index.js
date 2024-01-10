d02feebf67ad3dddf8c263bc36318a6e7bae168a
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2024-01-09 21:19:49 -0300

    Recebendo os dados do form

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs")
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false})) // Le oq foi enviado pelo formulário
+app.use(bodyParser.json()) // opcional
 
 
 app.get("/", (req, res) => {
@@ -13,9 +18,11 @@ app.get("/perguntar", (req, res) => {
 })
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulário recebido.")
+    let titulo = req.body.titulo
+    let desc = req.body.descricao
+    res.send(`Formulário recebido. ${titulo} descrição: ${desc}`)
 })
 
-app.listen(8080, () => {
+app.listen(8000, () => {
     console.log("App rodando!!")
 })
